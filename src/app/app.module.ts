1a72e7bf97726af2648bc45f2636f09c7a13752a
ErickInformatica <36520780+ErickInformatica@users.noreply.github.com>
2021-04-12 08:41:00 -0600

    First Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
